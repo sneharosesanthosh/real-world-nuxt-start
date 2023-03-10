6e52be46ba33fde5ae882056996d0bd54a94c7c3
sneha <inbox.sneha.santhosh@gmail.com>
2023-03-10 15:43:40 +0530

    add head()

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,17 @@
     <h1>Create an Event</h1>
   </div>
 </template>
+
+<script>
+export default{
+  head() {     // head() is a property used by vue-meta.
+    return ({
+      title: "Create an Event",
+      meta: [{
+        hid: 'Description',
+        name: 'Description',
+        content: 'Create an event to be conducted by everyone'
+      }]
+    })
+  }
+}</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,17 @@
     <h1>Events</h1>
   </div>
 </template>
+
+<script>
+export default{
+  head() {     // head() is a property used by vue-meta.
+    return ({
+      title: "Event Listing",
+      meta: [{
+        hid: 'Description',
+        name: 'Description',
+        content: 'These are the events taking place in the neighbourhood'
+      }]
+    })
+  }
+}</script>
