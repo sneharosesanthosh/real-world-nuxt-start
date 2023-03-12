8f1037e77013cacc7b2aeac5fc442cc11206d461
sneha <inbox.sneha.santhosh@gmail.com>
2023-03-12 21:15:20 +0530

    each individual event detail component

--- pages/events/_id.vue
+++ pages/events/_id.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>Events #{{ getEventId }}</h1>
+    <h1>Events #{{ event.id }}</h1>
+    <h3>Event: {{ event.title }}</h3>
   </div>
 </template>
 
@@ -9,13 +10,27 @@
 export default{
   head() {     // head() is a property used by vue-meta.
     return ({
-      title: "Event #"+this.getEventId,
+      title: "Event "+this.event.title,
       meta: [{
         hid: 'Description',
         name: 'Description',
-        content: 'This is event #'+this.getEventId
+        content: 'This is event: '+this.event.title
       }]
     })
+  },
+  async asyncData({$axios,error,params}) {
+    try {
+      const {data} = await $axios.get("https://localhost:3000/events/"+params.id)
+      return {
+        event: data
+      }
+    }
+    catch(e) {
+      error({
+        statusCod: 503,
+        message: "Unable to fetch event #"+params.id
+      })
+    }
   },
   computed: {
     getEventId() {
@@ -24,5 +39,4 @@ export default{
   }
 }</script>
 <style>
-
 </style>
