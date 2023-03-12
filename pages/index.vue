2f9cd14ed8c02a1fe2f5e37ed94b53819e98aab7
sneha <inbox.sneha.santhosh@gmail.com>
2023-03-12 07:59:18 +0530

    modify API call with async await

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,45 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard v-for="(event, index) in events" :key="index" :event="event"/>
   </div>
 </template>
 
 <script>
-export default{
-  head() {     // head() is a property used by vue-meta.
-    return ({
-      title: "Event Listing",
-      meta: [{
-        hid: 'Description',
-        name: 'Description',
-        content: 'These are the events taking place in the neighbourhood'
-      }]
-    })
-  }
-}</script>
+import EventCard from '~/components/EventCard.vue'
+export default {
+  async asyncData(context) {
+    try { 
+      const {data} = await context.$axios.get('http://localhost:3000/events')
+      return {
+        events: data,
+      }
+    }
+    catch(e) {
+      error({
+            statusCode: 503,
+            message: "Unable to fetch events data at this time.Please try again later."
+          })
+    }
+   
+  },
+  head() {
+    // head() is a property used by vue-meta.
+    return {
+      title: 'Event Listing',
+      meta: [
+        {
+          hid: 'Description',
+          name: 'Description',
+          content: 'These are the events taking place in the neighbourhood',
+        },
+      ],
+    }
+  },
+  components: {
+    EventCard,
+  },
+}
+</script>
+<style>
+</style>
